4515b87c99e20168b0234535b67392d893be32c2
Bujala Santosh Reddy <bujalasanthoshreddy@gmail.com>
2024-06-23 21:22:20 -0400

    Applied Active Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from '../shared/components/welcome/welcome.component';
 import { CategoryDetailsComponent } from '../shared/components/category-details/category-details.component';
+import { productDetailGuard } from '../shared/services/product-detail.guard';
 
 export const routes: Routes = [
     { path: 'welcome', component: WelcomeComponent },
     { path: 'details', component: CategoryDetailsComponent },
-    { path: 'category/:id', component: CategoryDetailsComponent },
+    { path: 'category/:id', canActivate: [productDetailGuard], component: CategoryDetailsComponent, },
     { path: '', redirectTo: 'welcome', pathMatch: 'full' },
     { path: '**', component: WelcomeComponent, pathMatch: 'full' }
 ];
